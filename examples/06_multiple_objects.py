3d0d423be7a9677b91169c740e10779c3186d9ec
Santos Alejandro Arellano Olarte <arellanosantoso6@gmail.com>
2024-08-07 10:16:51 -0600

    chore: Update file paths for texture and model resources

--- examples/06_multiple_objects.py
+++ examples/06_multiple_objects.py
@@ -15,7 +15,7 @@ pygame.display.set_mode((800, 800), flags=pygame.OPENGL | pygame.DOUBLEBUF, vsyn
 
 class TriangleGeometry:
     def __init__(self):
-        self.ctx = moderngl.get_context()
+        self.ctx = moderngl.create_context()
         vertices = np.array([
             0.0, 0.4, 0.0,
             -0.4, -0.3, 0.0,
@@ -30,43 +30,41 @@ class TriangleGeometry:
 
 class Mesh:
     def __init__(self, program, geometry):
-        self.ctx = moderngl.get_context()
+        self.ctx = moderngl.create_context()
         self.vao = geometry.vertex_array(program)
 
     def render(self, position, color, scale):
-        self.vao.program['position'] = position
-        self.vao.program['color'] = color
-        self.vao.program['scale'] = scale
+        self.vao.program['position'].value = position
+        self.vao.program['color'].value = color
+        self.vao.program['scale'].value = scale
         self.vao.render()
 
 
 class Scene:
     def __init__(self):
-        self.ctx = moderngl.get_context()
+        self.ctx = moderngl.create_context()
 
         self.program = self.ctx.program(
             vertex_shader='''
-                #version 330 core
+                #version 120
 
                 uniform mat4 camera;
                 uniform vec3 position;
                 uniform float scale;
 
-                layout (location = 0) in vec3 in_vertex;
+                attribute vec3 in_vertex;
 
                 void main() {
                     gl_Position = camera * vec4(position + in_vertex * scale, 1.0);
                 }
             ''',
             fragment_shader='''
-                #version 330 core
+                #version 120
 
                 uniform vec3 color;
 
-                layout (location = 0) out vec4 out_color;
-
                 void main() {
-                    out_color = vec4(color, 1.0);
+                    gl_FragColor = vec4(color, 1.0);
                 }
             ''',
         )
@@ -77,15 +75,15 @@ class Scene:
     def camera_matrix(self):
         now = pygame.time.get_ticks() / 1000.0
         eye = (math.cos(now), math.sin(now), 0.5)
-        proj = glm.perspective(45.0, 1.0, 0.1, 1000.0)
+        proj = glm.perspective(glm.radians(45.0), 1.0, 0.1, 1000.0)
         look = glm.lookAt(eye, (0.0, 0.0, 0.0), (0.0, 0.0, 1.0))
         return proj * look
 
     def render(self):
         camera = self.camera_matrix()
 
-        self.ctx.clear()
-        self.ctx.enable(self.ctx.DEPTH_TEST)
+        self.ctx.clear(0.9, 0.9, 0.9)
+        self.ctx.enable(moderngl.DEPTH_TEST)
 
         self.program['camera'].write(camera)
 
@@ -104,4 +102,116 @@ while True:
 
     scene.render()
 
+    pygame.display.flip()
+
+
+
+
+
+import math
+import os
+import sys
+
+import glm
+import moderngl
+import numpy as np
+import pygame
+
+os.environ['SDL_WINDOWS_DPI_AWARENESS'] = 'permonitorv2'
+
+pygame.init()
+pygame.display.set_mode((800, 800), flags=pygame.OPENGL | pygame.DOUBLEBUF, vsync=True)
+
+# Get the existing OpenGL context from Pygame
+ctx = moderngl.create_standalone_context()
+
+class TriangleGeometry:
+    def __init__(self, ctx):
+        self.ctx = ctx
+        vertices = np.array([
+            0.0, 0.4, 0.0,
+            -0.4, -0.3, 0.0,
+            0.4, -0.3, 0.0,
+        ], dtype='f4')
+        self.vbo = self.ctx.buffer(vertices.tobytes())
+
+    def vertex_array(self, program):
+        return self.ctx.vertex_array(program, [(self.vbo, '3f', 'in_vertex')])
+
+
+class Mesh:
+    def __init__(self, ctx, program, geometry):
+        self.ctx = ctx
+        self.vao = geometry.vertex_array(program)
+
+    def render(self, position, color, scale):
+        self.vao.program['position'].value = position
+        self.vao.program['color'].value = color
+        self.vao.program['scale'].value = scale
+        self.vao.render()
+
+
+class Scene:
+    def __init__(self, ctx):
+        self.ctx = ctx
+
+        self.program = self.ctx.program(
+            vertex_shader='''
+                #version 330
+
+                uniform mat4 camera;
+                uniform vec3 position;
+                uniform float scale;
+
+                in vec3 in_vertex;
+
+                void main() {
+                    gl_Position = camera * vec4(position + in_vertex * scale, 1.0);
+                }
+            ''',
+            fragment_shader='''
+                #version 330
+
+                uniform vec3 color;
+
+                out vec4 fragColor;
+
+                void main() {
+                    fragColor = vec4(color, 1.0);
+                }
+            ''',
+        )
+
+        self.triangle_geometry = TriangleGeometry(self.ctx)
+        self.triangle = Mesh(self.ctx, self.program, self.triangle_geometry)
+
+    def camera_matrix(self):
+        now = pygame.time.get_ticks() / 1000.0
+        eye = (math.cos(now), math.sin(now), 0.5)
+        proj = glm.perspective(glm.radians(45.0), 1.0, 0.1, 1000.0)
+        look = glm.lookAt(eye, (0.0, 0.0, 0.0), (0.0, 0.0, 1.0))
+        return proj * look
+
+    def render(self):
+        camera = self.camera_matrix()
+
+        self.ctx.clear(0.9, 0.9, 0.9)
+        self.ctx.enable(moderngl.DEPTH_TEST)
+
+        self.program['camera'].write(camera)
+
+        self.triangle.render((-0.2, 0.0, 0.0), (1.0, 0.0, 0.0), 0.2)
+        self.triangle.render((0.0, 0.0, 0.0), (0.0, 1.0, 0.0), 0.2)
+        self.triangle.render((0.2, 0.0, 0.0), (0.0, 0.0, 1.0), 0.2)
+
+
+scene = Scene(ctx)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    scene.render()
     pygame.display.flip()
--- examples/09_models_and_images.py
+++ examples/09_models_and_images.py
@@ -106,12 +106,13 @@ class Scene:
             ''',
         )
 
-        self.texture = ImageTexture('examples/data/textures/crate.png')
+    
+        self.texture = ImageTexture('Obed_Munoz/moderngl/examples/data/textures/crate.png')
 
-        self.car_geometry = ModelGeometry('examples/data/models/lowpoly_toy_car.obj')
+        self.car_geometry = ModelGeometry('Obed_Munoz/moderngl/examples/data/models/lowpoly_toy_car.obj')
         self.car = Mesh(self.program, self.car_geometry)
 
-        self.crate_geometry = ModelGeometry('examples/data/models/crate.obj')
+        self.crate_geometry = ModelGeometry('Obed_Munoz/moderngl/examples/data/models/crate.obj')
         self.crate = Mesh(self.program, self.crate_geometry, self.texture)
 
     def camera_matrix(self):
